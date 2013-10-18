1cc7aaf8b2145e5daab4b41350d3a34a7735a21e
Nikolay Botev <nbotev@Nikolays-MacBook-Air.local>
2013-10-17 17:32:13 -0700

    Initial Commit

---
+++ pipal/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  pipal
+//
+//  Created by Nikolay Botev on 10/17/13.
+//  Copyright (c) 2013 Nikolay Botev. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
