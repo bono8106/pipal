17bc9e43400d2e69bf4e9a229435bb9c3a47d0cc
Nikolay Botev <bono8106@yahoo.com>
2013-11-30 01:17:51 -0800

    Cleanup project for publishing

--- pipal/main.c
+++ pipal/main.c
@@ -9,10 +9,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
-char *pi = "1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989";
-
-int is_pal(char *x, int s, int len) {
+static int is_palindrome(char *x, int s, int len) {
     for (int i = 0; i < len/2; i++) {
         char c1 = x[s + i];
         char c2 = x[s + len - i - 1];
@@ -25,27 +24,42 @@ int is_pal(char *x, int s, int len) {
 
 int main(int argc, const char * argv[])
 {
-    int ll = 13;
-    int digits = 1000000;
+    // Inputs
+    int ll = 13; // Length of palindrome we are looking for
+    char *pi_file_name = "pi1000000.txt"; // File with pre-computed digits of pi
+    // Source: http://www.angio.net/pi/digits.html
 
-    pi = malloc(digits + 1);
+    printf("Loading pi...\n");
 
-    FILE *pifile = fopen("/Users/nbotev/Downloads/pi1000000.txt", "r");
-    fread(pi, digits, 1, pifile);
-    fclose(pifile);
+    // Open file with pi digits
+    FILE *pi_fp = fopen(pi_file_name, "r");
+    if (! pi_fp) {
+        printf("%s (%s)\n", strerror(errno), pi_file_name);
+        return 2;
+    }
+
+    // Determine the number of pi digits available
+    fseek(pi_fp, 0, SEEK_END); // seek to end of file
+    long digits = ftell(pi_fp); // get current file pointer
+    fseek(pi_fp, 0, SEEK_SET); // seek back to beginning of file
+
+    // Read pi digits into memory
+    char *pi = malloc(digits + 1);
+    fread(pi, digits, 1, pi_fp);
+    fclose(pi_fp);
     pi[digits] = 0;
 
-    for (int i = 800000; i < strlen(pi) - ll; i++) {
-        if (is_pal(pi, i, ll)) {
-            printf("found pal at pos %d\n", i);
+    // Look for palendrome
+    printf("Looking for %d-digit palindrome...\n", ll);
+    for (int i = 0; i < strlen(pi) - ll; i++) {
+        if (is_palindrome(pi, i, ll)) {
+            printf("Found %d-digit palindrome in pi at position %d\n", ll, i);
             char *pal = malloc(ll + 1);
             strncpy(pal, pi + i, ll);
             pal[ll] = 0;
-            printf("pal = %s\n", pal);
-            return 1;
+            printf("Palindrome = %s\n", pal);
+            return 0;
         }
     }
-    printf("Hello, World!\n");
-    return 0;
+    return 1;
 }
-
